a12a5b1938b610eee0b2bfa9289a425ebe711056
Ian_Edwards <edwards.ian.c@gmail.com>
2022-04-12 09:29:22 -0500

    logos and favicons added

---
+++ react-app/src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import "./styles/navigation.css"
+
+const Navigation = () => {
+    return(
+        <nav>
+            <h1 className="nav-header">It works</h1>
+        </nav>
+    )
+}
+
+export default Navigation;
