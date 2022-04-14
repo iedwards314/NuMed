b1b523cbceecaff8d60f76fee6af1f08caaa1e89
Ian_Edwards <edwards.ian.c@gmail.com>
2022-04-14 08:04:42 -0500

    insurance routes finished

--- app/api/insurance_routes.py
+++ app/api/insurance_routes.py
@@ -14,3 +14,39 @@ def get_insurance_policies(id):
 def get_insurance_policy(id):
     insurance_policy = Insurance_Policy.query.get(id)
     return insurance_policy.to_dict()
+
+@insurance_routes.route('/create', methods=['POST'])
+def create_project():
+    data = dict(request.json)
+    new_insurance_policy = Insurance_Policy(
+        user_id = data['user_id'],
+        insurance_co=data['insurance_co'],
+        subscriber_num=data['subscriber_num'],
+        group_num=data['group_num'],
+    )
+
+    db.session.add(new_insurance_policy)
+    db.session.commit()
+    return new_insurance_policy.to_dict()
+
+@insurance_routes.route('/delete/<int:id>', methods=['DELETE'])
+def delete_insurance_policy(id):
+    data = dict(request.json)
+    insurance_policy = Insurance_Policy.query.get(id)
+    res = {"id": id}
+    db.session.delete(insurance_policy)
+    db.session.commit()
+    return res
+
+@insurance_routes.route('/edit/<int:id>', methods=["PUT"])
+def edit_insurance_policy(id):
+    insurance_policy = dict(request.json)
+    # changed id to variable
+    data = Insurance_Policy.query.get(insurance_policy[id])
+
+    data.insurance_co = insurance_policy['insurance_co'],
+    data.subsriber_num = insurance_policy['subsriber_num'],
+    data.group_num = insurance_policy['group_num'],
+
+    db.session.commit()
+    return data.to_dict()
