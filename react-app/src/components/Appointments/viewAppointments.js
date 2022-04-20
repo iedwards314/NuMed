19f23112e945a113a26667da6432ce04ca79737a
Ian_Edwards <edwards.ian.c@gmail.com>
2022-04-20 17:39:03 -0500

    still errors in the appointment render

--- react-app/src/components/Appointments/viewAppointments.js
+++ react-app/src/components/Appointments/viewAppointments.js
@@ -1,35 +1,49 @@
-
+import React from "react";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { SessionCheck } from "../../utils/user"
-import {getAppointments} from "../../store/appointments.js"
+import { useDispatch, useSelector } from "react-redux";
+import { SessionCheck } from "../../utils/user";
 import { useParams } from "react-router-dom";
+import { getAppointments } from "../../store/appointments";
 
+const GetAllAppointments = () => {
+  const user = SessionCheck();
+  // console.log("user is...", user)
+  const dispatch = useDispatch();
+//   const [appts, setAppts] = useState({});
+  const { userId } = useParams();
 
-const GetAllAppointments = ({appointmentsObj}) => {
+  const apptsObj = useSelector((state) => state?.appointments);
+  let apptsArr;
+  if(apptsObj){
+      Object.values(apptsObj)
+  }
+  console.log("appts...", apptsArr)
 
-    const user = SessionCheck()
-    console.log("user is...", user)
-    const dispatch = useDispatch()
-    const {userId} = useParams()
-    const state = useState(state => state);
+//   useEffect(() => {
+//     (async () => {
+//       const response = await fetch(`/api/appointments/user/${userId}`);
+//       const aptList = await response.json();
+//       setAppts(aptList);
+//     })();
+//   }, [dispatch, userId]);
 
-    console.log("state in get all appointments is...", appointmentsObj)
+  useEffect(() => {
+    dispatch(getAppointments(userId));
+}, [dispatch, userId])
 
-
-    // useEffect(() => {
-    //     dispatch(getAppointments(+userId))
-    //     console.log("in use effect state",state);
-    //   }, [dispatch, userId])
+if(apptsArr && apptsArr.length > 0){
 
     return (
-        <>
-            <ul>
-                <li key="1">Got this rending no errors</li>
-            </ul>
-        </>
-    )
+      <>
+        <ul>
+          {apptsArr?.map((appt, idx) => {
+            <li key={idx}><p>{appt.doctor_info?.dr_last_name}</p></li>;
+          })}
+        </ul>
+      </>
+    );
+} else return ("hello")
 
-}
+};
 
 export default GetAllAppointments;
